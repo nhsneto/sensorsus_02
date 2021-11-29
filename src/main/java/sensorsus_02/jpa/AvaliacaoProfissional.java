db783ab66c347d80a0c0e1332a1aafafe03a3db3
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-11-29 16:35:26 -0300

    adiciona a classe AvaliacaoProfissional

--- src/main/java/sensorsus_02/jpa/AvaliacaoProfissional.java
+++ src/main/java/sensorsus_02/jpa/AvaliacaoProfissional.java
@@ -9,35 +9,17 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "TB_AVALIACAO_PACIENTE")
-public class AvaliacaoPaciente implements Serializable {
+@Table(name = "TB_AVALIACAO_PROFISSIONAL")
+public class AvaliacaoProfissional implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    // TODO implementar a relacao com Paciente
+    // TODO implementar a relacao com ProfissionalSaude
     
     // TODO implementar a relacao com Estabelecimento
     
-    // TODO implementar a relacao com Servico
-    
     @Column(name = "TXT_COMENTARIO", nullable = false, length = 255)
     private String comentario;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getComentario() {
-        return comentario;
-    }
-
-    public void setComentario(String comentario) {
-        this.comentario = comentario;
-    }
 }
