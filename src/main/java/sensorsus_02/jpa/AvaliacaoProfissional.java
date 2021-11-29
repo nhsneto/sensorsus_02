e3d6f7b64fe8ccc058e9181ed890c53f53a7b141
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-11-29 17:08:51 -0300

    add getters e setters em Eestabelecimento e AvaliacaoProfi.

--- src/main/java/sensorsus_02/jpa/AvaliacaoProfissional.java
+++ src/main/java/sensorsus_02/jpa/AvaliacaoProfissional.java
@@ -11,15 +11,31 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "TB_AVALIACAO_PROFISSIONAL")
 public class AvaliacaoProfissional implements Serializable {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     // TODO implementar a relacao com ProfissionalSaude
     
     // TODO implementar a relacao com Estabelecimento
     
     @Column(name = "TXT_COMENTARIO", nullable = false, length = 255)
     private String comentario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
 }
--- src/main/java/sensorsus_02/jpa/Estabelecimento.java
+++ src/main/java/sensorsus_02/jpa/Estabelecimento.java
@@ -11,13 +11,13 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "TB_ESTABELECIMENTO")
 public class Estabelecimento implements Serializable {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // TODO implementar relacionamento com Endereco    
-
+    // TODO implementar relacionamento com Endereco
+    
     // TODO implementar relacionamento com Servico
     
     // TODO implementar relacionamento com AvaliacaoPaciente
@@ -28,4 +28,28 @@ public class Estabelecimento implements Serializable {
     private String nome;
     @Column(name = "TXT_CODIGO_CNES", nullable = false, unique = true, length = 40)
     private String codigoCnes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigoCnes() {
+        return codigoCnes;
+    }
+
+    public void setCodigoCnes(String codigoCnes) {
+        this.codigoCnes = codigoCnes;
+    }
 }
