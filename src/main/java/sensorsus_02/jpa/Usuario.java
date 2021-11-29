67a64041dd13653ca5295ce8f3b13f9696564a86
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-11-29 15:24:24 -0300

    usuario: adiciona setters e getters

--- src/main/java/sensorsus_02/jpa/Usuario.java
+++ src/main/java/sensorsus_02/jpa/Usuario.java
@@ -29,4 +29,44 @@ public abstract class Usuario implements Serializable {
     protected Integer idade;
     @Column(name = "TXT_SENHA")
     protected String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
