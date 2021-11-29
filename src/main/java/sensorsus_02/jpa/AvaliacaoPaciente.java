89523d6490ab1cbe860a9e29c6484201466a0685
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-11-29 16:26:10 -0300

    adiciona a classe AvaliacaoPaciente

---
+++ src/main/java/sensorsus_02/jpa/AvaliacaoPaciente.java
@@ -0,0 +1,27 @@
+package sensorsus_02.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.eclipse.persistence.internal.jpa.parsing.GenerationContext;
+
+@Entity
+@Table(name = "TB_AVALIACAO_PACIENTE")
+public class AvaliacaoPaciente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    // TODO implementar a relacao com Paciente
+    
+    // TODO implementar a relacao com Estabelecimento
+    
+    // TODO implementar a relacao com Servico
+    
+    @Column(name = "TXT_COMENTARIO", nullable = false, length = 255)
+    private String comentario;
+}
