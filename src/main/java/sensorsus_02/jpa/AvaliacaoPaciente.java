0ff519d844fcca5501d9b032dad8b1643a2ddc8f
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-11-29 16:27:54 -0300

    AvaliacaoPaciente: adiciona serializable

--- src/main/java/sensorsus_02/jpa/AvaliacaoPaciente.java
+++ src/main/java/sensorsus_02/jpa/AvaliacaoPaciente.java
@@ -1,5 +1,6 @@
 package sensorsus_02.jpa;
 
+import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,7 +11,7 @@ import org.eclipse.persistence.internal.jpa.parsing.GenerationContext;
 
 @Entity
 @Table(name = "TB_AVALIACAO_PACIENTE")
-public class AvaliacaoPaciente {
+public class AvaliacaoPaciente implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
