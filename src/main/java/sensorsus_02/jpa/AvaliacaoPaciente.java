e2c23a260913af569f7934be1a7ba35fbe45c880
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-11-29 16:29:46 -0300

    AvaliacaoPaciente: adiciona setters e getters

--- src/main/java/sensorsus_02/jpa/AvaliacaoPaciente.java
+++ src/main/java/sensorsus_02/jpa/AvaliacaoPaciente.java
@@ -25,4 +25,20 @@ public class AvaliacaoPaciente implements Serializable {
     
     @Column(name = "TXT_COMENTARIO", nullable = false, length = 255)
     private String comentario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
 }
