acdc7ce41addbd79e6b76e349e94eb2edefb9b30
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-12-09 17:33:13 -0300

    Estabelecimento: adiciona relacionamento com endereco

--- src/main/java/sensorsus_02/jpa/Estabelecimento.java
+++ src/main/java/sensorsus_02/jpa/Estabelecimento.java
@@ -1,11 +1,15 @@
 package sensorsus_02.jpa;
 
 import java.io.Serializable;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,13 +21,16 @@ public class Estabelecimento implements Serializable {
     private Long id;
 
     // TODO implementar relacionamento com Endereco
-    
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "ID_ENDERECO", referencedColumnName = "ID")
+    private Endereco endereco;
+
     // TODO implementar relacionamento com Servico
     
     // TODO implementar relacionamento com AvaliacaoPaciente
     
     // TODO implementar relacionamento com AvaliacaoProfissional
-    
+
     @Column(name = "TXT_NOME", nullable = false, length = 255)
     private String nome;
     @Column(name = "TXT_CODIGO_CNES", nullable = false, unique = true, length = 40)
