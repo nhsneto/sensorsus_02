aa33e7465eea492d83fa08039ce528e5aaf0f5b9
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-12-10 00:16:42 -0300

    AvaliacaoProfissional: adiciona hashCode, Equals e toString

--- src/main/java/sensorsus_02/jpa/AvaliacaoProfissional.java
+++ src/main/java/sensorsus_02/jpa/AvaliacaoProfissional.java
@@ -61,4 +61,25 @@ public class AvaliacaoProfissional implements Serializable {
     public void setEstabelecimento(Estabelecimento estabelecimento) {
         this.estabelecimento = estabelecimento;
     }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof AvaliacaoProfissional)) {
+            return false;
+        }
+        AvaliacaoProfissional other = (AvaliacaoProfissional) object;
+        return !((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)));
+    }
+
+    @Override
+    public String toString() {
+        return "sensorsus_02.jpa.AvaliacaoProfissional[ id=" + id + " ]";
+    }
 }
