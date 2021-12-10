e14f874d2b358912715b2d0ca7a0883b283d05c0
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-12-10 00:24:50 -0300

    Servico: adiciona hashCode, Equals e toString

--- src/main/java/sensorsus_02/jpa/Servico.java
+++ src/main/java/sensorsus_02/jpa/Servico.java
@@ -69,4 +69,25 @@ public class Servico implements Serializable {
     public void setDepartamento(String departamento) {
         this.departamento = departamento;
     }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Servico)) {
+            return false;
+        }
+        Servico other = (Servico) object;
+        return !((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)));
+    }
+
+    @Override
+    public String toString() {
+        return "sensorsus_02.jpa.Servico[ id=" + id + " ]";
+    }
 }
