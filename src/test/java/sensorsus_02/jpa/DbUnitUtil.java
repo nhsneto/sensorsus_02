20d7088d7f7cd2a7ef040612c260ce95ef578e45
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-12-10 17:17:34 -0300

    adiciona a classe DbUnitUtil

---
+++ src/test/java/sensorsus_02/jpa/DbUnitUtil.java
@@ -0,0 +1,46 @@
+package sensorsus_02.jpa;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+
+public class DbUnitUtil {
+
+    private static final String XML_FILE = "/dbunit/dataset.xml";
+
+    public static void inserirDados() {
+        Connection conn = null;
+        IDatabaseConnection db_conn = null;
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:derby://localhost:1527/sensorsus_02", "app", "app");
+            db_conn = new DatabaseConnection(conn);
+            FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
+            builder.setColumnSensing(true);
+            InputStream in = DbUnitUtil.class.getResourceAsStream(XML_FILE);
+            IDataSet dataSet = builder.build(in);
+            DatabaseOperation.CLEAN_INSERT.execute(db_conn, dataSet);
+        } catch (SQLException | DatabaseUnitException ex) {
+            throw new RuntimeException(ex.getMessage(), ex);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+
+                if (db_conn != null) {
+                    db_conn.close();
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex.getMessage(), ex);
+            }
+        }
+    }
+}
