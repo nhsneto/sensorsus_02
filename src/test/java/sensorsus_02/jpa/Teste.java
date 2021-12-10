b0ea744a4e386e25e4753a6d7efa5d3f913a898d
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-12-10 17:36:40 -0300

    adiciona a classe abstrata Teste no pacote Test

---
+++ src/test/java/sensorsus_02/jpa/Teste.java
@@ -0,0 +1,43 @@
+package sensorsus_02.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public abstract class Teste {
+
+    protected static EntityManagerFactory emf;
+    protected EntityManager em;
+    protected EntityTransaction et;
+
+    @BeforeClass
+    public static void setUpClass() {
+        emf = Persistence.createEntityManagerFactory("exemplo_06");
+        DbUnitUtil.inserirDados();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        emf.close();
+    }
+
+    @Before
+    public void setUp() {
+        em = emf.createEntityManager();
+        et = em.getTransaction();
+        et.begin();
+    }
+
+    @After
+    public void tearDown() {
+        if (!et.getRollbackOnly()) {
+            et.commit();
+        }
+        em.close();
+    }
+}
