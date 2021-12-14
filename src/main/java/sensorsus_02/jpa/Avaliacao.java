1619fde855978b9443dfbce0e0da809e9bd1e344
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-12-13 21:27:43 -0300

    Avaliacao: adiciona cascade PERSIST no relacionamento usuario

--- src/main/java/sensorsus_02/jpa/Avaliacao.java
+++ src/main/java/sensorsus_02/jpa/Avaliacao.java
@@ -20,7 +20,7 @@ public class Avaliacao implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "ID_USUARIO", referencedColumnName = "ID")
     private Usuario usuario;
     
