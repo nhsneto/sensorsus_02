bced254a2cbc90326b3cf77e1d1f4209840cdabb
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-12-15 14:55:54 -0300

    ServicoTeste: adiciona o teste persistirServico

--- src/test/java/sensorsus_02/jpa/ServicoTeste.java
+++ src/test/java/sensorsus_02/jpa/ServicoTeste.java
@@ -1,5 +1,19 @@
 package sensorsus_02.jpa;
 
-public class ServicoTeste {
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ServicoTeste extends Teste {
     
+    @Test
+    public void persistirServico() {
+        Servico servico = new Servico();
+        servico.setNome("Coleta de sangue");
+        servico.setDepartamento("Enfermagem");
+        
+        em.persist(servico);
+        em.flush();
+        
+        assertNotNull(servico.getId());
+    }
 }
