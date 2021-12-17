d9b7e8e7356d10fd3b1facda372b2cda5892cc5f
Nelson Henrique <nelsonhenrique11@gmail.com>
2021-12-17 19:14:39 -0300

    Adiciona asserts e modifica o id

--- src/test/java/sensorsus_02/jpa/PacienteTeste.java
+++ src/test/java/sensorsus_02/jpa/PacienteTeste.java
@@ -90,20 +90,25 @@ public class PacienteTeste extends Teste {
         assertEquals("joao1234", paciente.getSenha());
     }
     
-//    @Test
-//    public void atualizarPacienteMerge(){
-//        Long id = 2L;
-//        Paciente paciente = em.find(Paciente.class, id);
-//        paciente.setNome("Laura");
-//        paciente.setEmail("laura@mail.com");
-//        paciente.setLogin("lab12");
-//        paciente.setSenha("laura1234");
-//        
-//        em.clear();
-//        em.merge(paciente);
-//        paciente = em.find(Paciente.class, id);
-//        assertEquals("Laura", paciente.getNome());
-//    }
+    @Test
+    public void atualizarPacienteMerge(){
+        Long id = 1L;
+        Paciente paciente = em.find(Paciente.class, id);
+        assertNotNull(paciente);
+        paciente.setNome("Laura");
+        paciente.setEmail("laura@mail.com");
+        paciente.setLogin("lab12");
+        paciente.setSenha("laura1234");
+        
+        em.clear();
+        em.merge(paciente);
+        
+        paciente = em.find(Paciente.class, id);
+        assertEquals("Laura", paciente.getNome());
+        assertEquals("laura@mail.com", paciente.getEmail());
+        assertEquals("lab12", paciente.getLogin());
+        assertEquals("laura1234", paciente.getSenha());
+    }
     
     @Test
     public void removerPaciente() {
